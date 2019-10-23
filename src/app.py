4a9538809f870285704ad029a07ea8df5d1d6be8
Soeren Wegener <wegener92@gmail.com>
2019-10-24 01:01:47 +0200

    Moved tasks into separate python files.

--- src/app.py
+++ src/app.py
@@ -1,5 +1,7 @@
+import argparse
 import logging
 import sys
+from typing import Optional
 
 from pydiscourse import DiscourseClient
 from pydiscourse.exceptions import DiscourseClientError
@@ -9,7 +11,7 @@ from time import sleep
 
 import locale
 import schedule
-import tasks
+import tasks.announce_plenum
 
 locale.setlocale(locale.LC_TIME, 'de_DE.UTF-8')
 
@@ -22,8 +24,21 @@ def test_login(client: DiscourseClient) -> None:
         sys.exit(-1)
 
 
+def disable_request(client: DiscourseClient, disable_verb: Optional[str] = None, disable_path: Optional[str] = None):
+    original_request = client._request
+
+    def new_request_fn(verb, path, *args, **kwargs):
+        if disable_verb and verb == disable_verb or disable_path and path == disable_path:
+            logging.info(f'Dry run. {verb} request to {path} was not made. Data:')
+            logging.info(kwargs.get('data'))
+            return {}
+        return original_request(verb, path, *args, **kwargs)
+
+    client._request = new_request_fn
+
+
 def schedule_jobs(client: DiscourseClient) -> None:
-    schedule.every().day.at('13:37').do(tasks.announce_plenum, client)
+    schedule.every().day.at('13:37').do(tasks.announce_plenum.main, client)
 
 
 def main():
@@ -31,8 +46,31 @@ def main():
         format='%(asctime)s - %(levelname)s: %(message)s', level=logging.INFO
     )
 
+    parser = argparse.ArgumentParser(
+        description='Updates voucher post, sends out vouchers via PM, receives returned vouchers via PM'
+    )
+    parser.add_argument('--dry', action='store_true', help='do not execute POST or PUT requests')
+    parser.add_argument('--run_task', type=str, help='runs a specific task immediately and stops afterwards')
+
+    args = parser.parse_args()
+
     client = DiscourseClient(**DISCOURSE_CREDENTIALS)
+    if args.dry:
+        disable_request(client, 'POST')
+        disable_request(client, 'PUT')
+
     test_login(client)
+
+    if args.run_task:
+        import importlib
+        try:
+            task = importlib.import_module(f'tasks.{args.run_task}')
+        except ModuleNotFoundError:
+            logging.error(f'Task "{args.run_task}" does not exist')
+            sys.exit(1)
+        logging.info(f'Running task "{args.run_task}"')
+        task.main(client)
+        sys.exit()
 
     schedule_jobs(client)
 
---
+++ src/tasks/announce_plenum.py
@@ -0,0 +1,50 @@
+import logging
+
+from pydiscourse import DiscourseClient
+
+from utils import render
+from datetime import timedelta, datetime
+from itertools import count
+from typing import Tuple, List
+import re
+
+
+def get_next_plenum_date(now: datetime) -> Tuple[datetime, timedelta]:
+    next_days = (now + timedelta(days=i) for i in count())
+    first_sundays = (x for x in next_days if x.weekday() == 6 and x.day <= 7)
+    plenum_date = next(first_sundays)
+    delta = plenum_date - now
+    return plenum_date, delta
+
+
+def topic_exists(title: str, topics: List[str]):
+    # Use the first 8 chars from the title, e.g: 2019-10-
+    # Then, there need to come any two digits (\d{2} - the formatting is a bit fucked up since we use formatstrings),
+    # followed by anything, as long as "plenum" appears anywhere
+    pattern = re.compile(fr'^{title[:8]}\d{{2}} .*plenum.*$', re.IGNORECASE)
+    matches = [pattern.search(t) for t in topics]
+    return any(matches)
+
+
+def main(client: DiscourseClient) -> None:
+    now = datetime.now()
+    plenum_date, delta = get_next_plenum_date(now)
+    if delta.days > 7:
+        logging.info(
+            f'Next plenum is too far in the future ({delta.days} days). Aborting.'
+        )
+        return
+    title = plenum_date.strftime('%Y-%m-%d Plenum')
+    topics = [
+        x['title'] for x in client.category_topics('orga/plena')['topic_list']['topics']
+    ]
+
+    if topic_exists(title, topics):
+        logging.info(f'"{title}" was already announced. Aborting.')
+        return
+
+    post_content = render('plenum.md', plenum_date=plenum_date)
+
+    # Category 23 == 'orga/plena'. But we must use the id here. D'oh!
+    client.create_post(post_content, category_id=23, title=title)
+    logging.info(f'Topic "{title}" created.')
