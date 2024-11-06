85033782cba5250f922b7d38dd10fa0b7eec41cb
Soeren Wegener <wegener92@gmail.com>
2024-11-06 18:09:22 +0100

    Fix plenum day I accidentally put thursday

--- src/tasks/plenum/__init__.py
+++ src/tasks/plenum/__init__.py
@@ -13,7 +13,7 @@ PLENUM_CATEGORY_NAME = "test" if DEBUG else "orga/plena"
 
 def get_next_plenum_date(now: datetime) -> Tuple[datetime, timedelta]:
     next_days = (now + timedelta(days=i) for i in count())
-    first_sundays = (x for x in next_days if x.weekday() == 3 and x.day <= 7)
+    first_sundays = (x for x in next_days if x.weekday() == 6 and x.day <= 7)
     plenum_date = next(first_sundays)
     delta = plenum_date - now
     return plenum_date, delta
