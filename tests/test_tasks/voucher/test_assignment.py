5c3733f95b2ad7642d70dd9d33b5aa64ef42ff25
Soeren Wegener <wegener92@gmail.com>
2026-02-28 01:33:56 +0100

    Add test for escalating the offer to the next person

--- tests/test_tasks/voucher/test_assignment.py
+++ tests/test_tasks/voucher/test_assignment.py
@@ -66,3 +66,72 @@ def test_offer_is_made(mocker, dummy_storage_client):
         ],
         "voucher_topics": {"40C3": 999},
     }
+
+
+def test_voucher_offer_escalation_in_queue(mocker, dummy_storage_client):
+    mocker.patch.object(
+        dummy_storage_client, "create_post", return_value={"topic_id": 456}
+    )
+
+    dummy_storage_client.storage.put(
+        "voucher",
+        {
+            "demand": {},
+            "queue": ["alice", "charlie", "bob"],
+            "voucher": [
+                {
+                    "index": 0,
+                    "voucher": "CHAOS123",
+                    "owner": None,
+                    "offered_to": [],
+                    "message_id": None,
+                    "history": [],
+                }
+            ],
+            "voucher_topics": {"40C3": 999},
+        },
+    )
+
+    # First call at T=0
+    with freezegun.freeze_time("2026-10-15T12:00:00+00:00"):
+        process_voucher_distribution(dummy_storage_client)
+        t0_offered_at = datetime.now(pytz.timezone("Europe/Berlin"))
+
+    assert len(dummy_storage_client.create_post.call_args_list) == 1
+    assert (
+        dummy_storage_client.create_post.call_args_list[0].kwargs["target_recipients"]
+        == "alice"
+    )
+
+    storage = dummy_storage_client.storage.get("voucher")
+    assert storage["voucher"][0]["offered_to"] == [
+        {"username": "alice", "offered_at": t0_offered_at.isoformat()}
+    ]
+    assert storage["queue"] == ["alice", "charlie", "bob"]
+
+    # Second call at T + 1 minute (no change)
+    with freezegun.freeze_time("2026-10-15T12:01:00+00:00"):
+        process_voucher_distribution(dummy_storage_client)
+
+    assert len(dummy_storage_client.create_post.call_args_list) == 1
+    assert dummy_storage_client.storage.get("voucher") == storage, (
+        "Storage should not change on second call within offer window"
+    )
+
+    # Third call at T + 3 hours + 1 minute (escalation to charlie)
+    with freezegun.freeze_time("2026-10-15T15:01:00+00:00"):
+        process_voucher_distribution(dummy_storage_client)
+        t1_offered_at = datetime.now(pytz.timezone("Europe/Berlin"))
+
+    assert len(dummy_storage_client.create_post.call_args_list) == 2
+    assert (
+        dummy_storage_client.create_post.call_args_list[1].kwargs["target_recipients"]
+        == "charlie"
+    )
+
+    final_storage = dummy_storage_client.storage.get("voucher")
+    assert final_storage["voucher"][0]["offered_to"] == [
+        {"username": "alice", "offered_at": t0_offered_at.isoformat()},
+        {"username": "charlie", "offered_at": t1_offered_at.isoformat()},
+    ]
+    assert final_storage["queue"] == ["alice", "charlie", "bob"]
